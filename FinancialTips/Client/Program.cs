6184b8a3340058f363974aad91ed83a66cd3dff4
WEE KAI QUAN KAVIAN 2105045E <2105045E@student.tp.edu.sg>
2023-02-07 20:03:43 +0800

    Finanlised

--- FinancialTips/Client/Program.cs
+++ FinancialTips/Client/Program.cs
@@ -1,3 +1,4 @@
+using FinancialTips.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +9,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace FinancialTips.Client
 {
@@ -18,10 +20,13 @@ namespace FinancialTips.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient("FinancialTips.ServerAPI",client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            builder.Services.AddHttpClient("FinancialTips.ServerAPI", (sp, client) => {client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);client.EnableIntercept(sp);}).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("FinancialTips.ServerAPI"));
 
+            builder.Services.AddHttpClientInterceptor();
+
+            builder.Services.AddScoped<HttpInterceptorService>();
 
             builder.Services.AddApiAuthorization();
 
